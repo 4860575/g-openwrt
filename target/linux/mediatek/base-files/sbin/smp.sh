c2c36aba84d2499410527cdec121fab3e5923c1d
536wfr <4860575@qq.com>
2023-10-28 19:09:32 +0800

    Revert "+"
    This reverts commit cf1364607d0476f5c2f7555fc2e11db3eabbfe15.

--- target/linux/mediatek/base-files/sbin/smp.sh
+++ target/linux/mediatek/base-files/sbin/smp.sh
@@ -719,13 +719,10 @@ setup_model()
 	*mt2500* |\
 	*zr-3020* |\
 	*360,t7* |\
-	abt,asr3000* |\
-	cmcc,a10* |\
 	xiaomi,mi-router-wr30u* |\
 	*rax3000m* |\
 	h3c,nx30pro |\
 	konka,komi-a31 |\
-	nradio,wt9103 |\
 	*7981*)
 		MT7981_whnat $num_of_wifi
 		;;
