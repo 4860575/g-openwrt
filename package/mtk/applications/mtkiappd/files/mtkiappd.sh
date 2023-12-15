fd7cb54fdb229089d8ffd60e785874154ce33240
Gavin <4860575@qq.com>
2023-12-16 01:03:06 +0800

    Update mtkiappd.sh

--- package/mtk/applications/mtkiappd/files/mtkiappd.sh
+++ package/mtk/applications/mtkiappd/files/mtkiappd.sh
@@ -7,10 +7,13 @@ USE_PROCD=1
 
 start_service() {
     procd_open_instance mtkiappd
-    procd_set_param command /usr/sbin/mtkiappd -e br-lan WIRELESS_IOCTL_PARAM >/dev/null
+    procd_set_param command /usr/sbin/mtkiappd -e br-lan -wi ra0 -wi rax0 && iwpriv rax0 set KickStaRssiLow=-75 && >/dev/null
     procd_set_param respawn
     procd_set_param netdev br-lan
     procd_set_param netdev ra0
+    procd_set_param netdev rax0
+    procd_set_param netdev apcli0
+    procd_set_param netdev apclix0
     procd_close_instance
 }
 
