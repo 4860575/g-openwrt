f6fefe8e6e35a9d7dc5e55d14dbf57265131ccc4
Gavin <4860575@qq.com>
2024-09-20 17:34:36 +0800

    ==

--- package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
+++ package/mtk/applications/luci-app-mtwifi-cfg/root/usr/share/luci-app-mtwifi-cfg/wireless-mtk.js
@@ -1380,11 +1380,11 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Flag, 'mumimo_dl', _('MU-MIMO DL'));
 					o.depends('mode', 'ap');
-					o.default = o.enabled;
+					o.default = o.disabled;
 
 					o = ss.taboption('advanced', form.Flag, 'mumimo_ul', _('MU-MIMO UL'));
 					o.depends('mode', 'ap');
-					o.default = o.enabled;
+					o.default = o.disabled;
 
 					o = ss.taboption('advanced', form.Flag, 'ofdma_dl', _('OFDMA DL'));
 					o.depends('mode', 'ap');
@@ -2032,9 +2032,10 @@ return view.extend({
 
 		s.handleRemove = function(section_id, radioNet, ev) {
 			var radioName = radioNet.getWifiDeviceName();
+			var ifmode = radioNet.getMode();
 			var hwtype = uci.get('wireless', radioName, 'type');
 
-			if (hwtype == 'mtwifi')
+			if (hwtype == 'mtwifi' && ifmode == 'ap')
 			{
 				var wifi_sections = uci.sections('wireless', 'wifi-iface');
 				var mbssid_num = 0;
