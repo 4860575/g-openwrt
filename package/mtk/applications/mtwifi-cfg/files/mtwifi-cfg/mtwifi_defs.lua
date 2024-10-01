1526e5a3cca489159601af28ab6a0f962c529aa2
Gavin <4860575@qq.com>
2024-10-01 08:54:21 +0800

    --

--- package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
+++ package/mtk/applications/mtwifi-cfg/files/mtwifi-cfg/mtwifi_defs.lua
@@ -16,7 +16,6 @@
 
 local mtwifi_defs = {}
 
-mtwifi_defs.dbdc_init_ifname = "ra0"
 mtwifi_defs.max_mbssid = 16
 mtwifi_defs.max_acl_entry = 129
 
@@ -50,6 +49,7 @@ mtwifi_defs.vif_cfgs = {
     ["RTSThreshold"] = "2347",
     ["HT_AMSDU"] = "1",
     ["HT_AutoBA"] = "1",
+    ["HT_BAWinSize"] = "256",
     ["HT_GI"] = "1",
     ["HT_LDPC"] = "1",
     ["HT_OpMode"] = "0",
@@ -62,14 +62,15 @@ mtwifi_defs.vif_cfgs = {
     ["VHT_LDPC"] = "1",
     ["VHT_SGI"] = "1",
     ["VHT_STBC"] = "1",
-    ["MuMimoDlEnable"] = "1",
-    ["MuMimoUlEnable"] = "1",
+    ["MuMimoDlEnable"] = "0",
+    ["MuMimoUlEnable"] = "0",
     ["MuOfdmaDlEnable"] = "1",
     ["MuOfdmaUlEnable"] = "1",
     ["DLSCapable"] = "0",
     ["WdsEnable"] = "0",
     ["WscConfMode"] = "0",
     ["TxRate"] = "0",
+    ["DtimPeriod"] = "1",
 }
 
 mtwifi_defs.vif_cfgs_idx = {
@@ -86,9 +87,7 @@ mtwifi_defs.vif_acl = {
 mtwifi_defs.chip_cfgs = {
     -- uci config = dat config, default value
     ["beacon_int"] = { "BeaconPeriod" , "100"},
-    ["dtim_period"] = { "DtimPeriod", "1"},
     ["whnat"] = { "WHNAT", "1"},
-    ["bandsteering"] = { "BandSteering", "0"},
 }
 
 mtwifi_defs.reinstall_cfgs = {
@@ -100,7 +99,6 @@ mtwifi_defs.iwpriv_ap_cfgs = {
     -- uci config = iwpriv set cmd , default value
     ["kicklow"] = {"KickStaRssiLow", "0"},
     ["assocthres"] = {"AssocReqRssiThres" , "0"},
-    ["steeringthresold"] = {"Steeringthresold" , "0"},
 }
 
 mtwifi_defs.enc2dat = {
@@ -110,13 +108,13 @@ mtwifi_defs.enc2dat = {
     ["sae-mixed"] = {"WPA2PSKWPA3PSK", "AES"},
     ["psk2+tkip+ccmp"] = {"WPA2PSK", "TKIPAES"},
     ["psk2+tkip+aes"] = {"WPA2PSK", "TKIPAES"},
-    ["psk2+tkip"] = {"WPA2PSK", "AES"},
+    ["psk2+tkip"] = {"WPA2PSK", "TKIP"},
     ["psk2+ccmp"] = {"WPA2PSK", "AES"},
     ["psk2+aes"] = {"WPA2PSK", "AES"},
     ["psk2"] = {"WPA2PSK", "AES"},
     ["psk+tkip+ccmp"] = {"WPAPSK", "AES"},
     ["psk+tkip+aes"] = {"WPAPSK", "AES"},
-    ["psk+tkip"] = {"WPAPSK", "TKIP"},
+    ["psk+tkip"] = {"WPAPSK", "AES"},
     ["psk+ccmp"] = {"WPAPSK", "AES"},
     ["psk+aes"] = {"WPAPSK", "AES"},
     ["psk"] = {"WPAPSK", "AES"},
