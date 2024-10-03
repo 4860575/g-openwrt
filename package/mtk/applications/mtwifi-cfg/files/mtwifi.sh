782f208fe2d52087aa48b403f9cad58e9d0aa7a3
Gavin <4860575@qq.com>
2024-10-03 22:55:42 +0800

    ==

--- package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+++ package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
@@ -7,40 +7,34 @@ append DRIVERS "mtwifi"
 
 detect_mtwifi() {
 	local idx ifname
-	local band htmode htbsscoex ssid dbdc_main
+	local band hwmode htmode htbsscoex ssid dbdc_main
 	if [ -d "/sys/module/mt_wifi" ]; then
 		dev_list="$(l1util list)"
 		for dev in $dev_list; do
 			config_get type ${dev} type
 			[ "$type" = "mtwifi" ] || {
 				ifname="$(l1util get ${dev} main_ifname)"
-
 				idx="$(l1util get ${dev} subidx)"
-				[ $idx -eq 1 ] && dbdc_main="1" || dbdc_main="0"
-
-				band="$(l1util get ${dev} band)"
-				if [ -z "$band" ] || [ "$band" = "nil" ]; then
-					[ $idx -eq 1 ] && band="2g" || band="5g"
-				fi
-
-				if [ "$band" = "2g" ]; then
+				if [ $idx -eq 1 ]; then
+					band="2g"
+					hwmode="11g"
 					htmode="HE40"
 					htbsscoex="1"
 					ssid="ImmortalWrt-2.4G"
-				elif [ "$band" = "5g" ]; then
+					dbdc_main="1"
+				else
+					band="5g"
+					hwmode="11a"
 					htmode="HE160"
 					htbsscoex="0"
 					ssid="ImmortalWrt-5G"
-				elif [ "$band" = "6g" ]; then
-					htmode="HE160"
-					htbsscoex="0"
-					ssid="ImmortalWrt-6G"
+					dbdc_main="0"
 				fi
-
 				uci -q batch <<-EOF
 					set wireless.${dev}=wifi-device
 					set wireless.${dev}.type=mtwifi
 					set wireless.${dev}.phy=${ifname}
+					set wireless.${dev}.hwmode=${hwmode}
 					set wireless.${dev}.band=${band}
 					set wireless.${dev}.dbdc_main=${dbdc_main}
 					set wireless.${dev}.channel=auto
@@ -50,7 +44,7 @@ detect_mtwifi() {
 					set wireless.${dev}.mu_beamformer=1
 					set wireless.${dev}.noscan=${htbsscoex}
 					set wireless.${dev}.serialize=1
-
+					
 					set wireless.default_${dev}=wifi-iface
 					set wireless.default_${dev}.device=${dev}
 					set wireless.default_${dev}.network=lan
@@ -63,3 +57,4 @@ EOF
 		done
 	fi
 }
+
